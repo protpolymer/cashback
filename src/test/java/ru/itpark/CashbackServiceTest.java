ea567736429d3eb2d8a4b31e001328c957bcfa90
protpolymer <nvsvkont@gmail.com>
2018-11-22 22:26:32 +0300

    added junit tests

--- src/test/java/ru/itpark/CashbackServiceTest.java
+++ src/test/java/ru/itpark/CashbackServiceTest.java
@@ -9,12 +9,46 @@ class CashbackServiceTest {
     @Test
     void cashbackCalculate() {
         {
-            // Positive test for cashback
 
             CashbackService cashbackTest = new CashbackService();
             int cashback = cashbackTest.cashbackCalculate(9_542, 12_500, 6_900);
 
             assertEquals(2790, cashback);
+        }
+        {
+
+            CashbackService cashbackTest = new CashbackService();
+            int cashback = cashbackTest.cashbackCalculate(0, 0, 0);
+
+            assertEquals(0, cashback);
+        }
+        {
+
+            CashbackService cashbackTest = new CashbackService();
+            int cashback = cashbackTest.cashbackCalculate(9_542, 0, 0);
+
+            assertEquals(95, cashback);
+        }
+        {
+
+            CashbackService cashbackTest = new CashbackService();
+            int cashback = cashbackTest.cashbackCalculate(0, 12_500, 0);
+
+            assertEquals(625, cashback);
+        }
+        {
+
+            CashbackService cashbackTest = new CashbackService();
+            int cashback = cashbackTest.cashbackCalculate(0, 0, 6_900);
+
+            assertEquals(2070, cashback);
+        }
+        {
+
+            CashbackService cashbackTest = new CashbackService();
+            int cashback = cashbackTest.cashbackCalculate(300_000, 60_000, 10_000);
+
+            assertEquals(3000, cashback);
         }
     }
 }
